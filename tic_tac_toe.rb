04382c92c4c047b80f31857f3ae45042bd7d45d7
yokn <discolouredattic@gmail.com>
2020-07-13 17:19:49 +0300

    need to access @player1 somehow

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -16,6 +16,7 @@
 class Player
   def initialize(piece)
     @piece = piece
+    puts @piece
   end
 end
 
@@ -25,25 +26,36 @@ class Board
     p @board_array
   end
 
-  def update_board; end
+  def set_current_player
+    p @player1
+    @current_player = @player1
+    p @current_player
+  end
+
+  def update_board
+    # @current_player=
+  end
 
   def add_piece(slot)
-    @board_array[slot] = 'X'
+    @board_array[slot] = @current_player.piece
   end
 end
 
 class Game
+  attr_accessor :player1
   def initialize
-    @board_array = Board.new
+    @board = Board.new
     @player1 = Player.new('X')
     @player2 = Player.new('O')
   end
 
   def start_new_game
+    p @player1
+    @board.set_current_player
     puts "X's Turn"
     puts 'Please enter a valid move'
     slot = gets.chomp.to_i
-    @board_array.add_piece(slot)
+    @board.add_piece(slot)
   end
 
   def check_game_over; end

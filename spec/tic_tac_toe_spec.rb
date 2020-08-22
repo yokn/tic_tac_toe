44c8844e77006edf313ae639410820148b5a509f
yokn <yokn@protonmail.com>
2020-08-22 17:59:39 +0300

    update tests

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,30 +2,27 @@
 
 require './tic_tac_toe'
 
-# rubocop:di sable Metrics/BlockLength
-describe 'tic_tac_toe' do
-  describe Game do
-    context 'when the game is underway' do
-      before(:example) do
-        @game = Game.new
-      end
+describe Game do
+  context 'when the game is underway' do
+    subject(:game) { Game.new }
+    context 'when a player has met the winning condition' do
       it 'ends the game when X has three in a row' do
-        @game.board.board_array = %w[X X X]
-        expect(@game.board.check_win('X')).to be true
+        game.board.board_array = %w[X X X]
+        expect(game.board.check_win('X')).to be true
       end
       it 'ends the game when O has three in a row' do
-        @game.board.board_array = %w[O O O]
-        expect(@game.board.check_win('O')).to be true
+        game.board.board_array = %w[O O O]
+        expect(game.board.check_win('O')).to be true
       end
-      context 'when adding a piece' do
-        it 'adds the right piece to the right slot' do
-          @game.board.add_piece('X', 7)
-          expect(@game.board.board_array[6]).to eql('X')
-        end
-        it 'does not add the piece if the selected slot is invalid' do
-          @game.board.board_array[6] = 'O'
-          expect(@game.board.check_slot_eligibility(7)).to be false
-        end
+    end
+    context 'when adding a piece' do
+      it 'adds the right piece to the right slot' do
+        game.board.add_piece('X', 7)
+        expect(game.board.board_array[6]).to eql('X')
+      end
+      it 'does not add the piece if the selected slot is invalid' do
+        game.board.board_array[6] = 'O'
+        expect(game.board.check_slot_eligibility(7)).to be false
       end
     end
   end
